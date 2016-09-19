c5803b0072af15eea729dff74f4fb2b05ea5a9c8
Moritz Milde <mmilde@ini.uzh.ch>
2016-09-19 09:24:59 +0200

    Cleaned working directory. Added 8 bit version of VGG and is currently being tested on Zemo. Wrote a script to parse high precision weights into the low precision blob structure. Wrote script to test trained net on top-1 and top-5 acc.

--- caffe-extra-shot/matlab/caffe_plot.m
+++ caffe-extra-shot/matlab/caffe_plot.m
@@ -47,6 +47,7 @@ function dataset_plotyy(ds1, ds2, x_limits, y_limits, experiment)
     ylabel(ax(1),ds1.y_label) 
     ylabel(ax(2),ds2.y_label) 
     ylim(ax(2),y_limits);
+    % ylim(ax(1), [0, 40]);
     xlim(x_limits);    
     title(experiment);
 end
--- python/caffe/imagenet/convert_weights.py
+++ python/caffe/imagenet/convert_weights.py
@@ -8,11 +8,13 @@ have 4 blobs weights in high and low precision and biases in high and low precis
 import numpy as np
 import caffe
 import time
-
+#import sys
+#sys.path.append("/opt/anaconda2/lib/python2.7/site-packages")
 
 caffe_root = '/home/moritz/Repositories/caffe_lp/'
 model_root = 'examples/low_precision/imagenet/models/'
-weight_root = '/home/moritz/Downloads/VGG16_tmp/'
+# weight_root = '/home/moritz/Downloads/VGG16_tmp/'
+weight_root = '/home/moritz/VGG16_tmp/'
 snapshot_root = '/media/moritz/Data/ILSVRC2015/Snapshots/'
 
 
@@ -25,50 +27,30 @@ prototxt_hp = caffe_root + model_root + 'VGG16_deploy.prototxt'
 prototxt_lp = caffe_root + model_root + 'LP_VGG16_5_10_deploy.prototxt'
 
 
-caffe.set_mode_cpu()
+caffe.set_mode_gpu()
+caffe.set_device(0)
 net_hp = caffe.Net(prototxt_hp, weights_hp, caffe.TEST)
 print('Doing forward pass...')
 net_hp.forward()
 print('Done.')
 
-weights = []
-biases = []
-time.sleep(5)
 caffe.set_mode_gpu()
 caffe.set_device(0)
 net_lp = caffe.Net(prototxt_lp, weights_lp, caffe.TEST)
 print('Doing forward pass...')
-time.sleep(5)
 net_lp.forward()
 print('Done.')
 
 
 for i, ldx in enumerate(net_hp.params.keys()):
-#     # fetch weights 0
-#     # fetch biases 1
     ldx_lp = net_lp.params.keys()[i]
-#     net_lp.params[ldx_lp][0].data = net_hp.params[ldx][0].data
-#     net_lp.params[ldx_lp][1].data = net_hp.params[ldx][0].data
-#     net_lp.params[ldx_lp][2].data = net_hp.params[ldx][1].data
-#     net_lp.params[ldx_lp][3].data = net_hp.params[ldx][1].data
-    print np.shape(net_hp.params[ldx][0].data)
-    print np.shape(net_lp.params[ldx_lp][0].data)
+    W = net_hp.params[ldx][0].data[...]
+    b = net_hp.params[ldx][1].data[...]
+    net_lp.params[ldx_lp][0].data[...] = W
+    net_lp.params[ldx_lp][1].data[...] = W
+    net_lp.params[ldx_lp][2].data[...] = b
+    net_lp.params[ldx_lp][3].data[...] = b
+    print ldx_lp, ldx
 
 
-# net_lp.save(weight_root + 'HP_VGG16.caffemodel')
-# print len(weights)
-# del net_hp
-
-
-# for i, ldx in enumerate(net_lp.params.keys()):
-#     # hp_weights = 0
-#     # lp weights = 1
-#     # hp biases = 2
-#     # lp_biases = 3
-#     # net_hp.params[ldx][0]
-#     net_lp.params[ldx][0].data = weights[i]
-#     net_lp.params[ldx][1].data = weights[i]
-#     net_lp.params[ldx][2].data = biases[i]
-#     net_lp.params[ldx][3].data = biases[i]
-
-#     print ldx
+net_lp.save(weight_root + 'HP_VGG16_v2.caffemodel')
