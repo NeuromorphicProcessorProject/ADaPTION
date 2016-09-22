fc224a47450fe1c7b806342b9720675297e0e84c
mmilde <mmilde@ini.uzh.ch>
2016-09-22 10:25:36 +0200

    Changed test_net.sh to perform rounding with different ratios of available number of bits

--- examples/low_precision/imagenet/test_net.sh
+++ examples/low_precision/imagenet/test_net.sh
@@ -3,6 +3,15 @@ CAFFE_DIR=../../..
 SOLVER_DIR=$CAFFE_DIR/examples/low_precision/imagenet/solver
 MODEL_DIR=$CAFFE_DIR/examples/low_precision/imagenet/models
 LOG_DIR=$CAFFE_DIR/examples/low_precision/imagenet/log
+NET_NAME=LP_VGG16_5_10_deploy.prototxt
+# NET_NAME=LP_VGG16_0_15_deploy.prototxt
+# NET_NAME=LP_VGG16_1_14_deploy.prototxt
+# NET_NAME=LP_VGG16_2_13_deploy.prototxt
+# NET_NAME=LP_VGG16_3_12_deploy.prototxt
+# NET_NAME=LP_VGG16_0_7_deploy.prototxt
+# NET_NAME=LP_VGG16_1_6_deploy.prototxt
+# NET_NAME=LP_VGG16_2_5_deploy.prototxt
+
 
 # $CAFFE_DIR/build/tools/caffe test \
 # 	--model $MODEL_DIR/VGG16_deploy.prototxt \
@@ -12,15 +21,16 @@ LOG_DIR=$CAFFE_DIR/examples/low_precision/imagenet/log
 # 	--log $LOG_DIR/
 
 $CAFFE_DIR/build/tools/caffe test \
-	--model $MODEL_DIR/VGG16_deploy_lp.prototxt \
-	--weights ../../../../../Downloads/VGG16_tmp/HP_VGG16.caffemodel \
+	--model $MODEL_DIR/$NET_NAME \
+	--weights ../../../../../Downloads/VGG16_tmp/HP_VGG16_v2.caffemodel \
 	--iterations 25000 \
 	--gpu 0 \ 
 	--log $LOG_DIR/
 
 # $CAFFE_DIR/build/tools/caffe test \
-# 	--model ../../../../../Downloads/VGG16_tmp/LP_VGG16_5_10_deploy.prototxt \
+# 	--model $MODEL_DIR/$NET_NAME \
 # 	--weights ../../../../../Downloads/VGG16_tmp/LP_VGG16.caffemodel \
 # 	--iterations 25000 \
 # 	--gpu 0 \
 #       --log $LOG_DIR/
+
