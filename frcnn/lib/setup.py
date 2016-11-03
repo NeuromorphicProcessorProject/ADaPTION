14414bc922929477842e54934fb0a69ba53a2574
enrico <enrico@localhost.localdomain>
2016-11-03 14:01:08 +0100

    Changed setup.py for Faster R-CNN to work in CPU only mode.

--- frcnn/lib/setup.py
+++ frcnn/lib/setup.py
@@ -55,7 +55,7 @@ def locate_cuda():
             raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
 
     return cudaconfig
-CUDA = locate_cuda()
+#CUDA = locate_cuda()
 
 
 # Obtain the numpy include directory.  This logic works across numpy versions.
@@ -87,7 +87,7 @@ def customize_compiler_for_nvcc(self):
     def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
         if os.path.splitext(src)[1] == '.cu':
             # use the cuda for .cu files
-            self.set_executable('compiler_so', CUDA['nvcc'])
+#            self.set_executable('compiler_so', CUDA['nvcc'])
             # use only a subset of the extra_postargs, which are 1-1 translated
             # from the extra_compile_args in the Extension class
             postargs = extra_postargs['nvcc']
@@ -122,23 +122,23 @@ ext_modules = [
         extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
         include_dirs = [numpy_include]
     ),
-    Extension('nms.gpu_nms',
-        ['nms/nms_kernel.cu', 'nms/gpu_nms.pyx'],
-        library_dirs=[CUDA['lib64']],
-        libraries=['cudart'],
-        language='c++',
-        runtime_library_dirs=[CUDA['lib64']],
-        # this syntax is specific to this build system
-        # we're only going to use certain compiler args with nvcc and not with
-        # gcc the implementation of this trick is in customize_compiler() below
-        extra_compile_args={'gcc': ["-Wno-unused-function"],
-                            'nvcc': ['-arch=sm_35',
-                                     '--ptxas-options=-v',
-                                     '-c',
-                                     '--compiler-options',
-                                     "'-fPIC'"]},
-        include_dirs = [numpy_include, CUDA['include']]
-    ),
+#    Extension('nms.gpu_nms',
+#        ['nms/nms_kernel.cu', 'nms/gpu_nms.pyx'],
+#        library_dirs=[CUDA['lib64']],
+#        libraries=['cudart'],
+#        language='c++',
+#        runtime_library_dirs=[CUDA['lib64']],
+#        # this syntax is specific to this build system
+#        # we're only going to use certain compiler args with nvcc and not with
+#        # gcc the implementation of this trick is in customize_compiler() below
+#        extra_compile_args={'gcc': ["-Wno-unused-function"],
+#                            'nvcc': ['-arch=sm_35',
+#                                     '--ptxas-options=-v',
+#                                     '-c',
+#                                     '--compiler-options',
+#                                     "'-fPIC'"]},
+#        include_dirs = [numpy_include, CUDA['include']]
+#    ),
     Extension(
         'pycocotools._mask',
         sources=['pycocotools/maskApi.c', 'pycocotools/_mask.pyx'],
